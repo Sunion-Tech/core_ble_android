7514b495d5450af4b1c87004ac9a700b68c7bbb7
Tom Chen <tom_chen@sunion.com.tw>
2023-11-14 14:18:38 +0800

    [Refactor] OTA function

--- src/main/java/com/sunion/core/ble/usecase/LockOTAUseCase.kt
+++ src/main/java/com/sunion/core/ble/usecase/LockOTAUseCase.kt
@@ -22,16 +22,17 @@ class LockOTAUseCase @Inject constructor(
     private val bleCmdRepository: BleCmdRepository,
     private val statefulConnection: ReactiveStatefulConnection
 ) {
-    suspend fun setOTAStatus(target:Int, state:Int, fileSize:Int): BleV2Lock.OTAStatus {
+    suspend fun setOTAStart(target:Int, fileSize:Int): BleV2Lock.OTAStatus {
         if (!statefulConnection.isConnectedWithDevice()) throw NotConnectedException()
-        val bytes = byteArrayOf(target.toByte()) + byteArrayOf(state.toByte()) + fileSize.toLittleEndianByteArray()
+        val startState = 2
+        val bytes = byteArrayOf(target.toByte()) + byteArrayOf(startState.toByte()) + fileSize.toLittleEndianByteArray()
         val sendCmd = bleCmdRepository.createCommand(
             function = 0xC3,
             key = statefulConnection.key(),
             data = bytes
         )
         return statefulConnection
-            .setupSingleNotificationThenSendCommand(sendCmd, "LockOTAUseCase.setOTAStatus")
+            .setupSingleNotificationThenSendCommand(sendCmd, "LockOTAUseCase.setOTAStart")
             .filter { notification ->
                 bleCmdRepository.isValidNotification(statefulConnection.key(), notification, 0xC3)
             }
@@ -45,15 +46,16 @@ class LockOTAUseCase @Inject constructor(
             }
             .flowOn(Dispatchers.IO)
             .catch { e ->
-                Timber.e("LockOTAUseCase.setOTAStatus exception $e")
+                Timber.e("LockOTAUseCase.setOTAStart exception $e")
                 throw e
             }
             .single()
     }
 
-    suspend fun setOTAFinish(target:Int, state:Int, fileSize:Int, iv:String, signature:String): BleV2Lock.OTAStatus {
+    suspend fun setOTAFinish(target:Int, fileSize:Int, iv:String, signature:String): BleV2Lock.OTAStatus {
         if (!statefulConnection.isConnectedWithDevice()) throw NotConnectedException()
-        val bytes = byteArrayOf(target.toByte()) + byteArrayOf(state.toByte()) + fileSize.toLittleEndianByteArray() +iv.hexToByteArray() + signature.hexToByteArray()
+        val finishState = 1
+        val bytes = byteArrayOf(target.toByte()) + byteArrayOf(finishState.toByte()) + fileSize.toLittleEndianByteArray() +iv.hexToByteArray() + signature.hexToByteArray()
         val sendCmd = bleCmdRepository.createCommand(
             function = 0xC3,
             key = statefulConnection.key(),
@@ -80,9 +82,10 @@ class LockOTAUseCase @Inject constructor(
             .single()
     }
 
-    suspend fun setOTACancel(target:Int, state:Int): BleV2Lock.OTAStatus {
+    suspend fun setOTACancel(target:Int): BleV2Lock.OTAStatus {
         if (!statefulConnection.isConnectedWithDevice()) throw NotConnectedException()
-        val bytes = byteArrayOf(target.toByte()) + byteArrayOf(state.toByte())
+        val cancelState = 0
+        val bytes = byteArrayOf(target.toByte()) + byteArrayOf(cancelState.toByte())
         val sendCmd = bleCmdRepository.createCommand(
             function = 0xC3,
             key = statefulConnection.key(),
